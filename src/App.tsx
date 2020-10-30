7777a6b69209965858a51fba6e42076f1f08fa51
Renan Tavares <renan_velox@hotmail.com>
2020-10-29 23:13:16 -0300

    Revert "Criando o Context inicial da aplicação"

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import GlobalStyle from './styles/global'
 import SigIn from './pages/SigIn/index'
 //import SigUp from './pages/SigUp/index'
-import AuthContext from './contexto/AuthContext'
 
 const App: React.FC = () => (
    <>
-      <AuthContext.Provider value={{name: 'Renan'}}>
-         <SigIn/>
-      </AuthContext.Provider>
+      <SigIn/>
       <GlobalStyle/>
    </>
 )
